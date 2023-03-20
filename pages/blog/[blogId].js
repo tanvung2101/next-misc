af9e54c512f5308fb689c7de1ee89665c350bdd8
Tấn Vững Liên <lientanvung2101@gmail.com>
2023-03-20 14:31:16 +0700

    Environment Variables

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -7,21 +7,29 @@ function Blog({ title, description }) {
         <title>{title}</title>
         <meta name="description" content={description} />
       </Head>
-      <h1 className="content">Article</h1>
+      <h1 className="content">
+        Env Analytics {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+      </h1>
     </>
   );
 }
 
 export default Blog;
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   return {
-    paths: [{params: {blogId: '1'}}],
+    paths: [{ params: { blogId: "1" } }],
     fallback: false,
-  }
+  };
 }
 
 export async function getStaticProps() {
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+
+  console.log(
+    `Connecting to database with username ${user} and password ${password}`
+  );
   return {
     props: {
       title: "Article Title",
